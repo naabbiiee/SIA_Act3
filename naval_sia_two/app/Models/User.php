4b5ce38f1576f0a3f4ffea10531cb90b41262945
Angel Rose Naval <79906935+naabbiiee@users.noreply.github.com>
2023-02-07 00:58:12 +0800

    second commit

---
+++ naval_sia_two/app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+ namespace App\Models;
+ 
+ use Illuminate\Database\Eloquent\Model;
+
+ class User extends Model{
+    
+
+    protected $table = 'tbluser';
+
+ // column sa table
+    protected $fillable = [
+        'username', 'password', 'gender', 'jobid'
+    ];
+
+    public $timestamps = false;
+    protected $primarykey = 'id';
+ }
